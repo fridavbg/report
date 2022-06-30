67d083f63ecf55ebcae6b84a5c1165aa21a0712c
fridavbg <fperssontech@gmail.com>
2022-06-30 17:56:35 +0200

    Documentation black jack init

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classes\Card\Deck;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,21 +19,40 @@ class GameController extends AbstractController
         $session->start();
         $session->clear();
 
+        if (!$session->get("leftOverDeck")) {
+            $session->set("leftOverDeck", new Deck());
+            $session->set("cardHand", [new Deck()]);
+        };
+
         $data = [
             'title' => 'Black jack'
         ];
-        return $this->render('game/game.html.twig', $data);
+        return $this->render('game/home.html.twig', $data);
     }
+
+    /**
+     * @Route("/game/doc", name="game-doc")
+     */
+    public function doc(): Response
+    {
+        $data = [
+            'title' => 'Black jack Documentation'
+        ];
+        return $this->render('game/doc.html.twig', $data);
+    }
+
 
     /**
      * @Route("/gamePlan", name="game-plan")
      */
     public function plan(SessionInterface $session): Response
     {
-
+        $deck = new Deck();
         $data = [
-            'title' => 'Start Black jack'
+            'title' => 'Start Black jack',
+            'cards' => $deck->getDeck(),
+            'cardHand' => $deck->getCardHand()
         ];
-        return $this->render('game/gamePlan.html.twig', $data);
+        return $this->render('game/plan.html.twig', $data);
     }
 }
