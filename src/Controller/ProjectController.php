94192e773a171b050e9faac38123e76af0cf3da5
fridavbg <fperssontech@gmail.com>
2022-08-16 19:55:44 +0200

    chart.js init

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 use App\Repository\PlasticProductionRepository;
 use App\Repository\SectorRepository;
 
@@ -12,15 +14,42 @@ class ProjectController extends AbstractController
 {
     #[Route('/proj', name: 'project')]
     public function index(
+        ChartBuilderInterface $chartBuilder,
         PlasticProductionRepository $plasticProductionRepository,
         SectorRepository $sectorRepository
     ): Response {
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+
+
         $plasticProduction = $plasticProductionRepository->findAll();
         $sector = $sectorRepository->findAll();
+
         $data = [
             'title' => 'MVC Kmom10',
             'plasticProduction' => $plasticProduction,
-            'sector' => $sector
+            'sector' => $sector,
+            'chart' => $chart,
         ];
         return $this->render('project/test.html.twig', $data);
     }
