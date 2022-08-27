a6ed3dfc4d26601999bb3967f95aef0b2bbf5c17
fridavbg <fperssontech@gmail.com>
2022-08-27 17:13:29 +0200

    login init

---
+++ src/Classes/Chart/Chart.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Classes;
+
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
+
+class ChartBuilder {
+
+    private $labels;
+    private $dataSet;
+    private $repository;
+
+    // public function __construct()
+    // {
+        
+    // }
+}
