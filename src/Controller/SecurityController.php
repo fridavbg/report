8d7b1a523a051536d5eac7867bccce8a1de63102
fridavbg <fperssontech@gmail.com>
2022-09-03 19:29:26 +0200

    login finished

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -51,6 +51,46 @@ class SecurityController extends AbstractController
     }
 
     /**
+     * @Route("/proj/admin/register/form", name="project_admin_register_form")
+     */
+    public function registerAsAdmin()
+    {
+        $data = [
+            'title' => 'Login MVC Kmom10',
+        ];
+        return $this->render('project/login/registerAsAdmin.html.twig', $data);
+    }
+
+        /**
+     * @Route("/proj/admin/register", name="project_admin_register_process")
+     */
+    public function registerAsAdminProcess(
+        Request $request,
+        ManagerRegistry $doctrine,
+    ): Response {
+        $entityManager = $doctrine->getManager();
+
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+        $image = $request->request->get('image');
+
+        $user = new User();
+        if (
+            is_string($username) and
+            is_string($password) and
+            is_string($image)
+        ) {
+            $user->setUsername($username);
+            $user->setRoles(["ROLE_USER"]);
+            $user->setPassword($password);
+            $user->setImage($image);
+        }
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return $this->redirectToRoute('project_users');
+    }
+
+    /**
      * @Route("/proj/register", name="project_register_process")
      */
     public function registerProcess(
@@ -85,11 +125,12 @@ class SecurityController extends AbstractController
     public function showAllUsers(
         UserRepository $userRepo
     ) {
-        $user = $this->getUser();
+        $admin = $this->getUser();
         $userRepo = $userRepo->findAll();
+
         $data = [
             'title' => 'Login MVC Kmom10',
-            'user' => $user,
+            'admin' => $admin,
             'allUsers' => $userRepo
         ];
         return $this->render('project/login/users.html.twig', $data);
@@ -111,6 +152,22 @@ class SecurityController extends AbstractController
             'admin' => $admin
         ];
         return $this->render('project/login/editProfileAsAdmin.html.twig', $data);
+    }
+
+    /**
+     * @Route("/proj/admin/erase/{username}", name="project_admin_erase")
+     */
+    public function eraseProfileAsAdmin(
+        ManagerRegistry $doctrine,
+        UserRepository $userRepo,
+        string $username
+    ) {
+        $entityManager = $doctrine->getManager();
+        $user = $userRepo->findOneBy(array('username' => $username));
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+        return $this->redirectToRoute('project_users');
     }
 
     /**
